a33cb7ac4c59e95fe0bb5ec1b1dc1062933b3c5e
DenzelFoxUA <canemidnight21@gmail.com>
2024-10-21 21:45:06 +0300

    Remake DataStructure

--- DoctorAppointmentDemo.Data/DB_Services/DB_DataStructure.cs
+++ DoctorAppointmentDemo.Data/DB_Services/DB_DataStructure.cs
@@ -5,35 +5,18 @@ namespace DoctorAppointmentDemo.Data.DB_Services
     public static class DB_DataStructure
     {   
         //набір классів для реалізації моделі даних налаштувань (JSON & XML settings files)
-        public class AppointmentsData : EntityData
-        {
-            public override int LastId { get; set; }
-            public override string Path { get; init; } = string.Empty;
-        }
 
         public class MainDB
         {
-            public DoctorsData? Doctors { get; set; }
-            public PatientsData? Patients { get; set; }
-            public AppointmentsData? Appointments { get; set; }
+            public EntityData? Doctors { get; set; }
+            public EntityData? Patients { get; set; }
+            public EntityData? Appointments { get; set; }
         }
 
-        public class DoctorsData : EntityData
+        public class EntityData
         {
-            public override int LastId { get; set; }
-            public override string Path { get; init; } = string.Empty;
-        }
-
-        public class PatientsData : EntityData
-        {
-            public override int LastId { get; set; }
-            public override string Path { get; init; } = string.Empty;
-        }
-
-        public abstract class EntityData
-        {
-            public abstract int LastId { get; set; }
-            public abstract string Path { get; init; }
+            public int LastId { get; set; }
+            public string Path { get; init; }
 
             public override string ToString()
             {
